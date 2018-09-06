fea273fcefe52cee33cbbda19c1c4fc57167cad7
Avision Ho <avision.ho@education.gov.uk>
2018-09-07 00:49:05 +0100

    test: include unit test to check func_plot_counts_percents

---
+++ tests/test_func_plot_counts_percents.R
@@ -0,0 +1,32 @@
+# ------------------ #
+# 0.2 - functionTest #
+# ------------------ #
+
+# ------------------------------------------
+# DESC: creates unit tests to ensure functions are working
+# SCRIPT DEPENDENCIES: none
+# PACKAGE DEPENDENCIES:
+# 1. 'pacman'
+# 2. 'magrittr'
+# 3. 'dplyr'
+# 4. 'tibble'
+
+# NOTES: none
+# ------------------------------------------
+vec_pacakges <- c("pacman", "magrittr", "dplyr", "ggplot2", "scales")
+pacman::p_load(char = vec_packages, install = TRUE)
+
+# UNIT TEST 1: [Field] = "Age"
+# For those who responded since those who didn't, will need to think of a smarter matching/joining method
+# such that we don't end up adding all 'prefer not to say/did not answer'
+x <- data_demographics_master %>% 
+  filter(Category == "Age" & Field != "prefer not to say / did not answer") %>% 
+  select(Field) %>% 
+  pull()
+ggplot(data = data.frame(x), mapping = aes(x = x)) +
+  geom_bar(mapping = aes(y = (..count..)), stat = "count", fill = "seagreen2") +
+  geom_text(mapping = aes(y = (..count..),
+                          label = ifelse((..count..) == 0, "",
+                                         scales::percent((..count..)/sum(..count..)))), 
+            stat="count", 
+            colour = "black")
