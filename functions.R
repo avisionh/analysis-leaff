9b732191f4f2f4941852cb1a6e484141df80e7b4
Avision Ho <avision.ho@education.gov.uk>
2018-09-06 12:59:34 +0100

    feat: add additional themes to plot such as title text

--- functions.R
+++ functions.R
@@ -21,14 +21,23 @@
  # 1. x | (tibble) dataframe to pass through plotting function
  # 2. colour_fill | (char) colour to fill the bars
  # 3. colour_txt | (char) colour for text of percents displayed
-func_plot_counts_percents <- function(x, colour_fill, colour_txt) {
+func_plot_counts_percents <- function(x, colour_fill, colour_txt, plot_title, plot_subtitle, plot_x) {
+  
   plot_bar <- ggplot(data = data.frame(x), mapping = aes(x=x)) +
     geom_bar(mapping = aes(y = (..count..)), fill = colour_fill) +
     geom_text(mapping = aes(y = (..count..),
                             label = ifelse((..count..) == 0, "",
                                            scales::percent((..count..)/sum(..count..)))), 
               stat="bin", 
-              colour = colour_txt)
+              colour = colour_txt) +
+    labs(title = plot_title, subtitle = plot_subtitle, x = plot_x, y = "Count") +
+    theme(plot.title = element_text(face = "bold", hjust = 0.5),
+          plot.subtitle = element_text(face = "bold", hjust = 0.5),
+          panel.background = element_blank(),
+          axis.line = element_line(color = "black")) +
+    # add text-wrapping for x labels
+    scale_x_discrete(labels = function(x) str_wrap(x, width = 10))
+  
   return(plot_bar)
 }
 
