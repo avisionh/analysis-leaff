2c3d356009a889c7e29064d8fab6a0c9d2c94ca0
Avision Ho <avision.ho@education.gov.uk>
2018-09-06 12:53:45 +0100

    feat: create user-defined function to plot bar chart with counts on y-axis and percents at top

---
+++ functions.R
@@ -0,0 +1,34 @@
+# ----------- #
+## Functions ##
+# ----------- #
+
+# ----------------------------------------
+# DESC: user-created functions for project
+# SCRIPT DEPENDENCIES: none
+# NOTES: none
+# ----------------------------------------
+
+
+# func_plot_counts_percents -----------------------------------------------
+# DESC: Creates bar chart that includes counts on y-axis and percents at top of bars
+# PACKAGE DEPENDENCIES:
+ # 1. ggplot2
+ # 2. scales
+# FUNCTION DEPENDENCIES: none
+# CREDIT: https://www.r-bloggers.com/bar-charts-with-percentage-labels-but-counts-on-the-y-axis/
+# NOTES: none
+# ARGUMENTS:
+ # 1. x | (tibble) dataframe to pass through plotting function
+ # 2. colour_fill | (char) colour to fill the bars
+ # 3. colour_txt | (char) colour for text of percents displayed
+func_plot_counts_percents <- function(x, colour_fill, colour_txt) {
+  plot_bar <- ggplot(data = data.frame(x), mapping = aes(x=x)) +
+    geom_bar(mapping = aes(y = (..count..)), fill = colour_fill) +
+    geom_text(mapping = aes(y = (..count..),
+                            label = ifelse((..count..) == 0, "",
+                                           scales::percent((..count..)/sum(..count..)))), 
+              stat="bin", 
+              colour = colour_txt)
+  return(plot_bar)
+}
+
