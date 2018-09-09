a280c541aec155df06315d390fcb12ff850adbb6
Avision Ho <avision.ho@education.gov.uk>
2018-09-09 13:30:07 +0100

    feat: add generalised function for plotting pie charts

--- scripts/0 - functions.R
+++ scripts/0 - functions.R
@@ -41,3 +41,52 @@ func_plot_counts_percents <- function(x, plot_stat = "count", colour_fill, colou
   return(plot_bar)
 }
 
+
+# func_plot_pie -----------------------------------------------
+# DESC: Creates pie chart
+# PACKAGE DEPENDENCIES:
+ # 1. ggplot2
+
+# FUNCTION DEPENDENCIES: none
+# CREDIT: https://www.r-bloggers.com/bar-charts-with-percentage-labels-but-counts-on-the-y-axis/
+# NOTES: none
+# ARGUMENTS:
+ # 1. x | (tibble) dataframe to pass through plotting function
+ # 2. colour_fill | (char) colour to fill the bars
+ # 3. colour_txt | (char) colour for text of percents displayed
+func_plot_pie <- function(x, col_counts, #col_category, 
+                          plot_title#, factor_levels
+                          ) {
+  plot_pie <- ggplot(data = x, 
+                     mapping = aes_string(x = "", y = col_counts
+                                          
+                                          #, #remove vars...
+                                   #fill = factor(x = col_category, levels = factor_levels), label = col_counts)
+                                   )) +
+    geom_bar(stat = "identity", width = 1) +
+    coord_polar(theta = "y", start = 0) +
+    geom_text(size = 3, position = position_stack(vjust = 0.5)) +
+    labs(title = paste0("Pie Chart: ", plot_title),
+         subtitle = "LEAF 2017",
+         fill = "Key") +
+    theme(plot.title = element_text(face = "bold", hjust = 0.5),
+          plot.subtitle = element_text(face = "bold", hjust = 0.5),
+          panel.background = element_blank(),
+          axis.line = element_blank(),
+          axis.title = element_blank(),
+          axis.text = element_blank())
+    
+  return(plot_pie)
+}
+
+func_plot_pie <- function(x, x_col = "", col_counts, col_category) {
+  ggplot(data = x, mapping = aes_string(x = x_col, y = col_counts, fill = col_category)) +
+    geom_bar(stat = "identity", width = 1) +
+    coord_polar(theta = "y", start = 0)
+}
+
+test_func <- function(x, col_counts, col_category) { 
+  ggplot(data = x, mapping = aes_string(x = col_counts, y = col_category)) +
+    geom_bar(stat = "identity")
+}
+test_func(x = data, col_counts = "Field", col_category = "Value")
