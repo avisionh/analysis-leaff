b0d1d44d8979e21cfdcf786d9a07388afd0c2a17
avisionh <a_vision@hotmail.co.uk>
2020-12-21 09:00:29 +0000

    refactor: Update to handle 2020 data
    This is different to previous years since the data was collected by Google Forms.

--- scripts/1.1 - dataImportClean.R
+++ scripts/1.1 - dataImportClean.R
@@ -19,6 +19,8 @@
 # Data import and wrangling
 library(googledrive)
 library(readr)
+library(dplyr)
+library(tidyr)
 
 drive_auth()
 
@@ -35,30 +37,10 @@ rm(file_temp, dl, out)
 # Data Clean --------------------------------------------------------------
 data_survey <- data_survey %>% 
   as_tibble() %>% 
-  # pad out tibble with previous row's entries
-  fill(x = c("UserID", "Film", "Question")) %>% 
-  # convert funny dates into ranges to replicate what's shown in the survey.
-  # Seems to be something funny when converting Excel to csv since '1-3' turns to '01-Mar'
-  mutate(Selection = case_when(
-    Selection == "01-Mar"    ~ "1-3",
-    Selection == "04-Jul"    ~ "4-7",
-    Selection == "Aug-13"    ~ "8-13",
-    TRUE                  ~ Selection
-  )) %>% 
-  # replace NA with 0 to mean they did not answer these sections
-  mutate(Response = ifelse(is.na(Response), 0, Response),
-         Film = as.factor(x = Film),
-         Question = as.factor(x = Question),
-         Selection = as.factor(x = Selection))
-
-
-# Interpretation-specific cleaning
-# DESC: Cleaning dataframe where we had to make some interpretations 
-#       when translating the 2017 version of the survey responses to 2018
-data_survey <- data_survey %>% 
-  mutate(Selection = str_replace_all(string = Selection, 
-                                  pattern = "The director\\(s\\)", 
-                                  replacement = "The director(s) and/or actor(s)"))
-
-# filter for latest year
-data_survey <- filter(.data = data_survey, YearOfFestival == year_latest)
+  mutate(Id = row_number()) %>% 
+  pivot_longer(cols = !c("Id", "Timestamp", "Which film screening did you attend?"),
+               names_to = "Question",
+               values_to = "Selection",
+               values_transform = list(Selection = as.character)) %>% 
+  # separate multiple-choice selection into rows
+  separate_rows("Selection", sep = ";")
